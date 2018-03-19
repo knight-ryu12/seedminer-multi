a9d0e43868ac1ce220a2ef7a6c9f97b3a50869cf
Pirater12 <pokemonemerald0@gmail.com>
2018-03-19 18:25:11 +0530

    Fix formatting

--- source/main.c
+++ source/main.c
@@ -14,41 +14,41 @@ void *thread(void *arg)
 {
 	uint8_t *arg2 = (uint8_t *)arg;
 	uint8_t buf[SHA_DIGEST_LENGTH];
-    SHA256_CTX c; //define only once at internal
-    char *a = "1";
+	SHA256_CTX c; //define only once at internal
+	char *a = "1";
 	sha256(&c,a,1,buf);
-    for(int i =0; i < SHA256_DIGEST_LENGTH; i++) {
-    	printf("%02X",buf[i]);
-    }
-    printf("thread %d\n", *arg2);
-    pthread_exit(arg2);
+	for(int i =0; i < SHA256_DIGEST_LENGTH; i++) {
+		printf("%02X",buf[i]);
+	}
+	printf("thread %d\n", *arg2);
+	pthread_exit(arg2);
 }
 
 int main(int argc, char **argv) 
 {
 	if(argc != 3)
 	{
-		printf("Wrong usage. seedminer <ID0> <KeyY>");
+		printf("Wrong usage. seedminer <ID0> <LFCS>");
 		exit(1);
 	}
 	
 	uint64_t ID0 = strtoul(argv[1], NULL, 16);
-	uint64_t KEY_Y = strtoul(argv[1], NULL, 16);
+	uint64_t LFCS = strtoul(argv[1], NULL, 16);
 	
-    pthread_t thread_t[8];
-    void *ret;
-    for(int i = 0; i < 8; i++)
-    {
-        if(pthread_create(&thread_t[i], NULL, thread, &i) != 0)
-        {
-            exit(1);
-        }
-        if(pthread_join(thread_t[i], &ret)!= 0)
-        {    
-            exit(2);
-        }
-        
-        printf("Thread %d returned %d\n", i, i);      
-    }
-    return 0;
+	pthread_t thread_t[8];
+	void *ret;
+	for(int i = 0; i < 8; i++)
+	{
+		if(pthread_create(&thread_t[i], NULL, thread, &i) != 0)
+		{
+			exit(1);
+		}
+		if(pthread_join(thread_t[i], &ret)!= 0)
+		{	 
+			exit(2);
+		}
+		
+		printf("Thread %d returned %d\n", i, i);
+	}
+	return 0;
 }
